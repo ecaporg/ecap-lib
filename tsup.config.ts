1e2330adf8c1b9e2574508eefc7142ab40635349
kytas999 <kytasevichvlad@gmail.com>
2025-09-18 23:26:18 +0300

    Update project metadata and add constants for filtering and sorting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  clean: true,
+  entry: [
+    "src/index.ts",
+    "src/constants/index.ts",
+    "src/utils/index.ts",
+    "src/types/index.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
+  outDir: "dist",
+  clean: true,
+  splitting: false,
+  shims: true,
 });
